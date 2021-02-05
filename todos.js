405999c2777695a46eea8e943b731352ed7cec90
Peter Jang <peter.x.jang@gmail.com>
2021-02-04 19:36:41 -0600

    Add todos with web request example

---
+++ todos.js
@@ -0,0 +1,20 @@
+/* global Vue, axios */
+
+var app = new Vue({
+  el: "#app",
+  data: function () {
+    return {
+      message: "Hello to my todo app!",
+      todos: [],
+    };
+  },
+  methods: {
+    loadTodos: function () {
+      console.log("loadTodos");
+      axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+        console.log(response);
+        this.todos = response.data;
+      });
+    },
+  },
+});
